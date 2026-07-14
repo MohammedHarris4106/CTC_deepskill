1cd1cc49d2e39a5975fb830314b3d269034fcfb3
MohammedHarrisN <mohammedharrisn.23csd@kongu.edu>
2026-07-14 10:12:12 +0530

    Added week 1 assignment

---
+++ Week 1/Week 1/Mockito/MyServiceTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+    @Test
+    public void testExternalApi() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+        System.out.println(result);
+        assertEquals("Mock Data", result);
+    }
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
